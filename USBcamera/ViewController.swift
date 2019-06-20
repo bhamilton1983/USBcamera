af9d1b9e5b6ad85d3ec37bc82b1522cfc811f6fa
Brian Hamilton <bhamilton@intertest.com>
2019-06-20 09:14:28 -0400

    Initial Commit

---
+++ USBcamera/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  USBcamera
+//
+//  Created by Brian Hamilton on 6/20/19.
+//  Copyright © 2019 Brian Hamilton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
